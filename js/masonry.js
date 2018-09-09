d53ff91980563c96084a514448e0a71a7e7a6526
ne_andrey <sorokinac@gmail.com>
2018-09-09 19:48:33 +0500

    first_imposition

---
+++ js/masonry.js
@@ -0,0 +1,19 @@
+//masonry.
+var totalRows = 3,
+    itemCol = 0;
+for(var rowCount = 0; rowCount < totalRows; rowCount++){
+    newCol = document.createElement('div');
+    newCol.className = 'col';
+    document.getElementsByClassName('images_content')[0].appendChild(newCol);
+}
+
+for(var itemCount = 0; itemCount <  document.getElementsByClassName('item').length; itemCount++){
+    document.getElementsByClassName('col')[itemCol].appendChild(document.getElementsByClassName('item')[0]);
+    if(itemCol < totalRows - 1){
+        itemCol++;
+    } else {
+        itemCol = 0;
+    }
+}
+
+//--------------------------------------------------------------------
